ea5e1b2c60a12adcfab489d3cac70e5b4a7a6474
Krurmi33 <2397429787@qq.com>
2023-06-05 01:43:55 +0800

    6.5 2460

---
+++ 2023.06/2460/src/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+        }
+        int[] res = new int[n];
+        int p = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) res[p++] = nums[i];
+        }
+        return res;
+    }
+}
